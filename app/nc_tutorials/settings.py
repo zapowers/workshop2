0403411946dcecc0beaa8533482dcccfa29749d5
Zachary Powers <zachapowers@gmail.com>
2023-04-22 12:24:55 -0500

    new change

--- app/nc_tutorials/settings.py
+++ app/nc_tutorials/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'django-insecure-(j-#aqq@vs$3l#0(031z4_h(_*pa&ye#dt&x69_@(&a6(=+fpg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [' ec2-18-118-96-122.us-east-2.compute.amazonaws.com','0.0.0.0', 'localhost', '127.0.0.1']
-
+ALLOWED_HOSTS = ['ec2-18-118-96-122.us-east-2.compute.amazonaws.com',
+                 '0.0.0.0', 'localhost', '127.0.0.1']
 
 # Application definition
 
@@ -128,7 +128,7 @@ USE_TZ = True
 
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-else: 
+else:
     STATIC_ROOT = [os.path.join(BASE_DIR, "static")]
 
 STATIC_URL = '/static/'
